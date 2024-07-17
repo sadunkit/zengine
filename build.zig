2556a9d6ef653281a720897a9b01f6fa569fe066
sadunkit <sadun@kityazilim.com>
2024-07-17 23:42:43 +0300

    initial files

---
+++ build.zig
@@ -0,0 +1,23 @@
+const std = @import("std");
+const os = std.os;
+// const target = b.standardTargetOptions(.{});
+
+pub fn build(b: *std.Build) !void {
+    const version = b.option([]const u8, "version", "application version string") orelse "0.0.1";
+    const app = b.option([]const u8, "app", "application name") orelse "zrrp";
+    const optimize = b.standardOptimizeOption(.{});
+
+    const options = b.addOptions();
+    options.addOption([]const u8, "version", version);
+
+    const subpath = try std.fmt.allocPrint(b.allocator, "{s}.zig", .{app});
+
+    const exe = b.addExecutable(.{
+        .name = app,
+        .root_source_file = b.path(subpath),
+        .target = b.host,
+        .optimize = optimize,
+    });
+
+    b.installArtifact(exe);
+}
