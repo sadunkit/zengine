b9bafb51d47d657ea26c0c7d551425e29ecde3c1
sadunkit <sadun@kityazilim.com>
2024-07-19 13:39:51 +0300

    add target options

--- build.zig
+++ build.zig
@@ -6,6 +6,7 @@ pub fn build(b: *std.Build) !void {
     const version = b.option([]const u8, "version", "application version string") orelse "0.0.1";
     const app = b.option([]const u8, "app", "application name") orelse "zrrp";
     const optimize = b.standardOptimizeOption(.{});
+    const target = b.standardTargetOptions(.{});
 
     const options = b.addOptions();
     options.addOption([]const u8, "version", version);
@@ -15,7 +16,7 @@ pub fn build(b: *std.Build) !void {
     const exe = b.addExecutable(.{
         .name = app,
         .root_source_file = b.path(subpath),
-        .target = b.host,
+        .target = target,
         .optimize = optimize,
     });
 
